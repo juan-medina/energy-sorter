ec7e342b4added0b9b2df82a446b03eb6a3c3e57
Juan Medina <jamedina@gmail.com>
2025-12-03 10:17:01 +0000

    refactor: add debug assertions in BatteryNode

--- src/nodes/BatteryNode.cs
+++ src/nodes/BatteryNode.cs
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2025 Juan Medina
 // SPDX-License-Identifier: MIT
 
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using EnergySorter.model;
 using Godot;
 
@@ -9,15 +11,15 @@ namespace EnergySorter.nodes;
 
 public partial class BatteryNode : Area2D
 {
-    private Battery _battery;
+	private Battery _battery;
 	private readonly List<Sprite2D> _energySprites = [];
 
 	public override void _Ready()
 	{
 		_battery = new Battery();
-		for(var i = 0; i < Battery.MaxEnergy; i++)
+		for (var i = 0; i < Battery.MaxEnergy; i++)
 		{
-			_energySprites.Add(GetNode<Sprite2D>($"Body/Energy{i+1}"));
+			_energySprites.Add(GetNode<Sprite2D>($"Body/Energy{i + 1}"));
 		}
 	}
 
@@ -30,16 +32,20 @@ public partial class BatteryNode : Area2D
 	private void UpdateVisuals()
 	{
 		var energies = _battery.Energies;
+		Debug.Assert(energies.Length <= Battery.MaxEnergy,
+			$"Update Visuals: A battery can not have more than {Battery.MaxEnergy} energy");
 
-		foreach(var sprite in _energySprites)
+		foreach (var sprite in _energySprites)
 		{
 			sprite.Visible = false;
 			sprite.Modulate = Colors.White;
 			sprite.Visible = false;
 		}
-		var idx = 0;
-		foreach (var color in energies)
+
+		var limit = Math.Min(energies.Length, Battery.MaxEnergy);
+		for (var idx = 0; idx < limit; idx++)
 		{
+			var color = energies[idx];
 			var sprite = _energySprites[idx];
 			sprite.Modulate = color;
 			sprite.Visible = true;
