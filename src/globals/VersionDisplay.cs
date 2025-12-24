85e83f2d96711a4378896d08cfc1467dc5a0196a
Juan Medina <jamedina@gmail.com>
2025-12-24 23:40:08 +0000

    Enhance version display with colored segments
    Updated VersionDisplay to show each part of the version string in a different color using BBCode. Adjusted the scene to enable BBCode and tweaked label positioning. Bumped project version to 0.0.1.0022.

--- src/globals/VersionDisplay.cs
+++ src/globals/VersionDisplay.cs
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: 2025 Juan Medina
 // SPDX-License-Identifier: MIT
 
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
 using Godot;
 
 namespace EnergySorter.globals;
@@ -8,13 +11,27 @@ namespace EnergySorter.globals;
 public partial class VersionDisplay : CanvasLayer
 {
 	private RichTextLabel _label;
+	private static readonly List<string> Colors = ["#F000F0", "#FF0000", "#FFA500", "#FFFF00", "#00FF00"];
 
 	public override void _Ready()
 	{
 		var version = ProjectSettings.GetSetting("application/config/version").AsString();
+		var parts = version.Split('.');
+		Debug.Assert(parts.Length == 4, "Version string does not have four parts.");
+		if (parts.Length != 4) return;
+
+		var text = new StringBuilder("v.");
+
+
+		for (var i = 0; i < parts.Length; i++)
+		{
+			text.Append(GetBbcodeTextColor(parts[i], Colors[i]));
+			if (i < parts.Length - 1) text.Append('.');
+		}
 
 		_label = GetNode<RichTextLabel>("Text");
-
-		_label.Text = $"v{version}";
+		_label.Text = text.ToString();
 	}
+
+	private static string GetBbcodeTextColor(string part, string color) => $"[color={color}]{part}[/color]";
 }
